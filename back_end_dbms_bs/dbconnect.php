48059bf6ee951d284dc8dd464ee6c7de70f2e404
root <navinash47@gmail.com>
2020-03-24 09:48:40 +0530

    database connected to 'BMS'database in php admin

--- back_end_dbms_bs/dbconnect.php
+++ back_end_dbms_bs/dbconnect.php
@@ -1,1 +1,11 @@
-
+<?php
+  $host = "localhost";
+  $user = "root";
+  $dbpass = "";
+  $dbname = "BMS";
+  // $name="avinash";
+  // $email="navinash47@gmail.com";
+  // $password="chintu2000";
+  $con = mysqli_connect($host,$user,$dbpass,$dbname);
+  
+?>

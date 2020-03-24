b84c5aab586b08edc30df85ba5ac1fce71f5075a
Avinash Nandyala <navinash47@gmail.com>
2020-03-24 07:37:53 +0530

    home.php and user login.php

---
+++ back_end_dbms_bs/home.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>Bus Management system | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+    />
+  </head>
+  <body>
+    <h1>BUS MANAGEMENT SYSTEM</h1>
+    <div class="options">
+      <a class="=home" href="./home.php">HOME</a>
+      <a class="=book_ticket" href="./book_ticket.html">BOOK TICKET</a>
+      <a class="=Register_admin" href="./reg_admin.html">REGISTER ADMIN</a>
+      <a class="=Register_user" href="./reg_user.html">REGISTER USER</a>
+      <a class="=Login_admin" href="./log_admin.html">LOGIN ADMIN</a>
+      <a class="=Login_user" href="./log_user.html">LOGIN USER</a>
+      <a class="=contactus" href="./contact_us.html">Contact US</a>
+      <a class="=help" href="./help.html">HELP</a>
+    </div>
+    <div class="Welcome"><h1>WELCOME</h1></div>
+  </body>
+</html>

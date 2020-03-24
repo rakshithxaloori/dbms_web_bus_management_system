e0944a0fcba8ef3c316f53438208057be95be065
root <navinash47@gmail.com>
2020-03-24 08:31:03 +0530

    regadmin written

--- back_end_dbms_bs/reg_admin.php
+++ back_end_dbms_bs/reg_admin.php
@@ -1,1 +1,56 @@
 
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="layoutsstyle.css">
+	<title>AdminSignup</title>
+	<style>
+		
+			a:link, a:visited {
+  background-color: #2196F3;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 25px;
+  margin-right:  10px;
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    font-family: "Comic Sans MS", cursive, sans-serif;
+}
+a:hover, a:active {
+  background-color: #0b7dda;
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+		
+	</style>
+</head>
+  
+    <body>
+    	<div class="header">
+    		<h1>Bus Booking Platform</h1>
+    		<p>Please Provide the Following Details to  Admin SignUp</p>
+    		
+    	</div>
+	<center>
+		
+		<form action = "signup-backend.php" method = "POST" class="container">
+			<label>Name:</label>
+			<input name = "name" placeholder = "Name" type="text"><br><br>
+            <label>Age:</label>
+			<input name = "age" placeholder = "Age" type="text"><br><br>
+            <label>Gender:</label>
+			<input name = "gender" placeholder = "gender" type="text"><br><br>
+            <label>Contact:</label>
+			<input name = "contact" placeholder = "Contact" type="text"><br><br>
+        
+			<label>Email:</label>
+			<input name = "email" placeholder = "Email" type="email"><br><br>
+			<label>Password:</label>
+			<input name = "password" type = "password" placeholder = "Password"><br><br>
+			<button class="btn success"type = "submit">Submit</button>
+			<button class="btn danger"type = "reset">ClearEntries</button>
+		</form>
+	</center>
+</div>
+</body>
+</html>

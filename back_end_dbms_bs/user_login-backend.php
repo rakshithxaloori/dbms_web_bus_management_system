5d519199c248c0deca901ab489f0bf0cba4b6477
root <navinash47@gmail.com>
2020-03-25 23:58:16 +0530

    userlogin finished

--- back_end_dbms_bs/user_login-backend.php
+++ back_end_dbms_bs/user_login-backend.php
@@ -1,1 +1,24 @@
+<?php
 
+require_once 'dbconnect.php';
+$username = $_POST["username"];
+$password = $_POST["password"];
+$query = "SELECT * FROM admin WHERE username='$username' and password='$password'";
+
+$result = mysqli_query($con, $query);
+$row=mysqli_fetch_array($result);
+$numResults = mysqli_num_rows($result);
+
+if($numResults == 1)
+{
+	$query = "UPDATE admin SET user_login_count = user_login_count + 1 WHERE username='$username'";
+	mysqli_query($con, $query);
+	require_once"usershome.php";
+
+}
+else
+{
+	echo "<br><br><br><center><h1>Invalid credentials!</h1></center>";
+}
+
+?>

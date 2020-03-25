e50b0842a90489d7036531d65c37cd00a40993f4
root <navinash47@gmail.com>
2020-03-26 00:17:12 +0530

    adminhome started

--- back_end_dbms_bs/adminshome.php
+++ back_end_dbms_bs/adminshome.php
@@ -1,1 +1,34 @@
 
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="layoutsstyle.css">
+	<title>Bus Mangement System | Adminhome</title>
+	<style>
+		
+			a:link, a:visited {
+  background-color: #2196F3;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 25px;
+  margin-right:  10px;
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    font-family: "Comic Sans MS", cursive, sans-serif;
+}
+a:hover, a:active {
+  background-color: #0b7dda;
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+		
+	</style>
+</head>
+  
+    <body>
+    	<div class="header">
+    		<h1>Bus Booking Platform<-Adminhome</h1>
+    	</div>
+</div>
+</body>
+</html>

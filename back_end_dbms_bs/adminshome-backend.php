c37cad03611987b8140b3d200d858918c81a555c
root <navinash47@gmail.com>
2020-03-26 13:44:35 +0530

    errors rectified in admin_login-backend.php

---
+++ back_end_dbms_bs/adminshome-backend.php
@@ -0,0 +1,24 @@
+<?php 
+require_once "dbconnect.php";
+// session_start();
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+$admin=$_SESSION['a_username'];
+
+$query = "SELECT * FROM admins WHERE username='$admin'";
+$result = mysqli_query($con, $query);
+$row=mysqli_fetch_array($result);
+$numResults = mysqli_num_rows($result);
+
+
+if($numResults==1){
+    echo "Hi, " .$row[2] . "!";
+    require_once "adminshome.php";
+}
+
+} 
+else {
+    echo "Please log in first to see users page.";
+    require_once "admin_login.php";
+}
+?>

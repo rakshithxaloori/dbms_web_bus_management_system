ab4e387033bd797d4bbb3a1f8d0ac43fb0775ba7
root <navinash47@gmail.com>
2020-03-27 13:59:29 +0530

    add_route added succesfully

---
+++ back_end_dbms_bs/add_route-backend.php
@@ -0,0 +1,36 @@
+<?php 
+require_once "dbconnect.php";
+session_start();
+if(isset($_SESSION['a_loggedin']) && $_SESSION['a_loggedin'] == true){
+    $route_id=$_POST["route_id"];
+    if(count($_POST['station_code'])<2){
+        echo "please enter atleast 2 stations!";
+        //require_once "add_route.php";
+    }
+
+    else{
+        for($i = 0; $i < count($_POST['station_code']); $i++)
+        {
+            $station_code = mysqli_real_escape_string($con, $_POST['station_code'][$i]);
+            $dist_from_source=mysqli_real_escape_string($con, $_POST['distance_from_source'][$i]);
+            $time_from_source=mysqli_real_escape_string($con, $_POST['time_from_source'][$i]);
+            $created_by=$_SESSION["a_username"];
+
+            // if (empty(trim($station))) continue;
+
+            $sql = "INSERT INTO Route(Route_id, station_code, dis_from_source_in_km, time_from_source_in_min,created_by)
+                    VALUES('$route_id', '$station_code', '$dist_from_source', '$time_from_source','$created_by')";
+            $SQ=mysqli_query($con, $sql);
+            echo'<center><h2 style="color:white;background-color:#333;">Route Successfully Registered</h2></center>';
+
+            if($SQ)require_once "adminshome.php";
+            else echo("Error description: ".mysqli_error($con));
+        }
+    }
+}
+
+else{
+    echo "Please log in first to see users page.";
+    require_once "admin_login.php";
+}
+?>
--- back_end_dbms_bs/add_route.php
+++ back_end_dbms_bs/add_route.php
@@ -3,6 +3,8 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="layoutsstyle.css">
 	<title>Bus Mangement System | AddRoute</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
 	<style>
 		
 			a:link, a:visited {
@@ -53,10 +55,122 @@ a:hover, a:active {
 
     </div>
 
-    <form action="add_route-backend.php" method="POST" class ="a_adddetails">
-        <label>Route id :</label>
-        <input name = "route_id" placeholder = "Route id" type="number"><br><br>
-        <button class="btn success" type = "submit">create bus</button>   <button class="btn danger" type = "reset">Clear</button>
-    </form>
+    <!-- <form action="add_route-backend.php" method="POST" class ="a_adddetails">
+        <center>
+          <label>Route id :</label>
+          <input name = "route_id" placeholder = "Route id" type="number"><br><br>
+          <form name="add_station" class="a_add_new_station" id ="add_station">
+            <table class="table_bordered" id="dynamic_field">
+              <tr>
+                <td><input type ="text" name="station[]" id="station" class="form-control station"></td>
+                <td><input type ="number" name="dis_from_source[]" id="dist_from_source" class="form-control dis_from_source"></td>
+                <td><input type ="number" name="time_from_source[]" id="time_from_source" class="form-control time_from_source"></td>
+  
+                <td><button name="add" id="add" class="addmore">+</button></td>
+              </tr>
+            </table>  
+          </form>
+          <input type ="text" name="destination" class="finalstation">
+          <input type ="number" name="final_distance" class="final_distance">
+          <input type ="number" name="final_time" class="final_time"><br>
+
+          <button class="btn success" type = "submit" class ="btn btn-info" id ="submit">create Route</button>   <button class="btn danger" type = "reset">Clear</button>
+        </center>
+    </form> -->
+    <form id="route_details">
+      <center>
+            <div class="field-wrap">
+                <label>
+                    Route_id<span class="req">*</span>
+                </label>
+                <input type="number" required autocomplete="off" name="route_id"/>
+            </div>
+
+            <table class="table table-bordered" id="dynamic_field">
+                <tr>
+                    <td>
+                        <!--div class="top-row"-->
+                        
+                        <div class="field-wrap">
+                            <label>
+                                Station Code<span class="req">*</span>
+                            </label>
+                            <input type="text"required autocomplete="off" name="station_code[]"/>
+                        </div>
+
+                        <div class="field-wrap">
+                            <label>
+                                Distance From Source<span class="req">*</span>
+                            </label>
+                            <input type="number"required autocomplete="off" name="distance_from_source[]"/>
+                        </div>
+
+                        <div class="field-wrap">
+                            <label>
+                                Time From Source<span class="req">*</span>
+                            </label>
+                            <input type="number"required autocomplete="off" name="time_from_source[]"/>
+                        </div>
+                    </td>
+                    <td><button type="button" name="add" id="add" class="btn btn-success">+</button></td>
+                </tr>
+            </table>
+            <input type="button" name="submit" id="submit"  class="btn btn-info" value="Submit" />
+            </center>
+        </form>
 </body>
 </html>
+<!-- <script>
+$(document).ready(function(){
+  var i=1;
+  $('#add').click(function(){
+    i++;
+    $('#dynamic_field').append('<tr id="row'+i+'"><td><input type ="text" name="station[]" id="station" class="form-control station"></td> <td><input type ="number" name="dis_from_source[]" id="dist_from_source" class="form-control dis_from_source"></td>  <td><input type ="number" name="time_from_source[]" id="time_from_source" class="form-control time_from_source"></td> <td><button name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+  });
+  $(document).on('click','.btn_remove', function(){
+                var button_id = $(this).attr("id");
+                $("#row"+button_id+"").remove();
+            });
+  $('#submit').click(function(){
+      $.ajax({
+          async: true,
+          url: "add_route-backend.php",
+          method: "POST",
+          data: $('#add_station').serialize(),
+          success:function(rt)
+          { 
+              alert(rt);
+              $('#add_station')[0].reset();
+          }
+      });
+  });
+});
+</script> -->
+<script>
+  $(document).ready(function(){
+      var i = 1;
+      $('#add').click(function(){
+          i++;
+          $('#dynamic_field').append('<tr id="row'+i+'"><td><div class="field-wrap"><label>Station Code<span class="req">*</span></label><input type="text"required autocomplete="off" name="station_code[]"/></div><div class="field-wrap"><label>Distance From Source<span class="req">*</span></label><input type="number"required autocomplete="off" name="distance_from_source[]"/></div><div class="field-wrap"><label>Time From Source<span class="req">*</span></label><input type="number"required autocomplete="off" name="time_from_source[]"/></div></td></td><td><button name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+      });
+
+      $(document).on('click','.btn_remove', function(){
+          var button_id = $(this).attr("id");
+          $("#row"+button_id+"").remove();
+      });
+
+      $('#submit').click(function(){
+          $.ajax({
+              async: true,
+              url: "add_route-backend.php",
+              method: "POST",
+              data: $('#route_details').serialize(),
+              success:function(rt)
+              {
+                  alert(rt);
+                  $('#route_details')[0].reset();
+              }
+          });
+      });
+  });
+</script>
